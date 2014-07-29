675da394b5a7d79bd2605c7ad36413d6a4526ad9
seleti <seleti@g.clemson.edu>
2014-07-29 00:15:27 -0500

    (high_voltage) implemented high_volate gem for root path

---
+++ config/initializers/high_voltage.rb
@@ -0,0 +1,6 @@
+# should deprecate this later.
+# config/initializers/high_voltage.rb
+HighVoltage.configure do |config|
+  config.home_page = 'home'
+  config.route_drawer = HighVoltage::RouteDrawers::Root
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get ':action' => 'static#:action'
+  get 'pages/home' => 'high_voltage/pages#show', id: 'home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
